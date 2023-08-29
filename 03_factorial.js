79020eb21c44f57831f815b63a450d552b057186
Andrei Gunderin <ga@web-notes.info>
2023-08-30 02:57:40 +0300

    chore: chapter #3

---
+++ 03_factorial.js
@@ -0,0 +1,32 @@
+const factorialRecursion = (n) => {
+
+	if (n < 0) return new Error(`Nummber couldn\'t be less then zero`);
+
+	if (n === 0) return 1;
+
+	if (n === 1) return 1;
+
+	return factorialRecursion(n - 1) * n;
+
+};
+
+const factorialLoop = (n) => {
+
+	if (n < 0) return new Error(`Nummber couldn\'t be less then zero`);
+
+	if (n === 0) return 1;
+
+	if (n === 1) return 1;
+
+	let result = 1;
+
+	for (let i = 2; i <= n; i += 1) {
+		result *= i;
+	}
+
+	return result;
+
+};
+
+console.log(factorialRecursion(5));
+console.log(factorialLoop(5));
